ed83dc750b1f8d81c1cda672879c027d14e53ed3
theolivenbaum <rafael@curiosity.ai>
2022-04-20 21:18:47 +0200

    missing ;

--- wiktionary-sharp/Program.cs
+++ wiktionary-sharp/Program.cs
@@ -55,7 +55,7 @@ using (var db = RocksDb.Open(options, dbPath, families))
         }
     }
 
-    app.MapGet("/api/languages", () => languages)
+    app.MapGet("/api/languages", () => languages);
     app.MapGet("/api/lang/{lang}/word/{word}", (string word, string lang) => db.Get(Encoding.UTF8.GetBytes($"{Limit(lang,100)}-{Limit(word, 2048)}"), WordDefinitionListSerializer.Instance, wordColumn));
     app.MapGet("/api/ping", () => "pong");
 
